80ba3eebfc2b44addce82de0e3ec76bbc81fdfb0
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-03-24 15:53:58 +0100

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,1 +1,7 @@
-module.exports = {};
+const drooM = require("droom.js");
+
+function messageDelete(droom, param1, param2, param3) { // Feel free to modify the parameter names and the variable names
+  const client = droom._client;
+}
+
+module.exports = messageDelete;
