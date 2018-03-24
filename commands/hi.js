4f6bd9f0c8595f618d8a0e211efd53accdf0fc20
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-03-24 15:45:16 +0100

    Create hi.js

---
+++ commands/hi.js
@@ -0,0 +1,14 @@
+const drooM = require("droom.js");
+
+function hi(droom, message, args) {
+  const client = droom._client;
+  const embed = new drooM.Embed();
+  embed.addInlineField(`Hi ${message.author.username}!`);
+  embed.setAuthor("name", client.user.username);
+  embed.setAuthor("icon_url", client.user.avatarURL);
+  embed.setDescription("Nice to see you!");
+  message.channel.createMessage({ embed: embed });
+}
+
+module.exports.run = hi;
+module.exports.path = __dirname + "/hi.js";
