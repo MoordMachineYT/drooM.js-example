b099501ec3caa0c48883cace43eb8da56d9d064e
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-03-24 15:39:57 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,9 @@
-function ping(client, message, args) {
+const drooM = require("droom.js");
+
+function ping(droom, message, args) {
+  const client = droom._client;
   message.channel.createMessage("Pong!");
 }
 
 module.exports.run = ping;
+module.exports.path = __dirname + "/ping.js";
