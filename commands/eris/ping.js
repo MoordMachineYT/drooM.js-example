e289bc513f584babd7aea32908c5edadf9e89caf
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-03-25 16:52:26 +0200

    Update ping.js

--- commands/eris/ping.js
+++ commands/eris/ping.js
@@ -2,7 +2,9 @@ const drooM = require("droom.js");
 
 function ping(droom, message, args) {
   const client = droom._client;
-  message.channel.createMessage("Pong!");
+  let embed = new drooM.Embed();
+  embed.addField("Pong!", `Ping: ${droom.ping}ms.`);
+  droom.send(message.channel.id, { embed: embed });
 }
 
 module.exports.run = ping;
