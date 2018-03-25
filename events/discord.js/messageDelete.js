917d94aebdfe84c84670dd0bf3584898af4066a8
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-03-25 16:01:50 +0200

    Create messageDelete.js

---
+++ events/discord.js/messageDelete.js
@@ -0,0 +1,7 @@
+const drooM = require("droom.js");
+
+function messageDelete(droom, param1, param2, param3) { // Feel free to edit these
+  const client = droom._client;
+}
+
+module.exports = messageDelete;
