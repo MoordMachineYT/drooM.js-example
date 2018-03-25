b34884c8dbfa72ef9ba8e81422a20ef4688912b6
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-03-25 16:57:12 +0200

    Update refresh.js

--- commands/discord.js/refresh.js
+++ commands/discord.js/refresh.js
@@ -1,1 +1,11 @@
-module.exports = {};
+const drooM = require("droom.js");
+
+function refresh(droom, message, args) {
+  const client = droom._client;
+  if (args === "refresh") return droom.send(message.channel.id, "You can't refresh the refresh command.");
+  const result = droom.refresh(args);
+  droom.send(message.channel.id, result);
+}
+
+module.exports.run = refresh;
+module.exports.path = __dirname + "/refresh.js";
